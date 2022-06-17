0a67dd988dc64980edf4d8d7974e8ad084921c31
Anele Madonda <anelemlaba413@gmail.com>
2022-06-17 13:51:47 +0200

    errors removed and unnecesary gibberish removed

--- ATM System/src/MainClass/Main.java
+++ ATM System/src/MainClass/Main.java
@@ -6,12 +6,9 @@ import GUI.MyFrame;
 
 
 public class Main {
-	//public static MyFrame frame;
+	public static MyFrame frame;
 	public static void main(String [] args) 
 	{
-		//frame = new MyFrame();
-		Optional<String> middleName = Optional.of("cabby");
-		System.out.println(middleName.toString()
-				);
+		frame = new MyFrame();
 	}
 }
